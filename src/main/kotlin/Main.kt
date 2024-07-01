1c4efa60b4b301624a126dac5dc78c4a3241c91b
AntonVzdor <der_nom@mail.ru>
2024-07-01 17:31:59 +0300

    закончил на 3.3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,30 @@
 package org.example
 
 fun main() {
-    println("Start")
+    daysAndWeeks()
+}
+
+fun centimeInMet(){
+    println("Укажите данные в сантиметрах")
+    val centimeters = readln().toInt()
+    val meters = (centimeters.toDouble()/100)
+    println("В метрах это: $meters")
+}
+
+fun kilogramInHundred(){
+    println("Укажите данные в килограммах")
+    val kilogram = readln().toInt()
+    val hundredweight = (kilogram.toDouble()/100)
+    println("В цейтнерах это: $hundredweight")
+}
+
+fun daysAndWeeks(){
+    println("Сколько дней прошло?")
+    val day = readln().toInt()
+    val week = day/7
+    println("Это $week недель.")
+}
+
+fun appleAndHuman(apple: Int, human: Int){
+
 }
